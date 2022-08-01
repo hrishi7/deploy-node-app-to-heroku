59d1048799de5b0d2f723d27409b6c41a6aace4f
Hrishikesh <hrishikesh@Hrishikeshs-MacBook-Pro.local>
2022-08-01 23:54:14 +0530

    change in code

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 dotenv.config();
 
 app.get("/health-checkup", (req, res) => {
-  res.status(200).json({ success: true, message: "server is alive", 'database Url': process.env.MONGO_URI });
+  res.status(200).json({ success: true, message: "server is alive updated", 'database Url': process.env.MONGO_URI });
 });
 
 app.listen(process.env.PORT || 5000, () => console.log(`server is listening`));
